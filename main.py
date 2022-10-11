1e6ea48ed8aac2221ca836d2fc201c860fc1a3f2
sarah castorena <s.castorena@outlook.com>
2022-10-11 13:03:59 -0700

    EX 2

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 # ******************************
 # Make your Code
 # ******************************
+# Sarah Castorena
+# SID 2028049
+import random
+numbers = []
+count = 0
+while count < 10:
+    random.seed()
+    randNum = random.randint(1,100)
+    numbers.append(randNum)
+    count = count + 1
+minNum = min(numbers)  
+minIndex = numbers.index(minNum)  
+print(minNum)
+print(minIndex)
